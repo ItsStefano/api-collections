12d6738a43914f8b92e85f1d6a4faa4242b36e48
Lucas Stefano <lk.stefano1@gmail.com>
2021-03-25 22:52:48 -0300

    Update to minOrNull and maxOrNull

--- src/main/kotlin/one/digitalinnovation/collections/Operações.kt
+++ src/main/kotlin/one/digitalinnovation/collections/Operações.kt
@@ -9,8 +9,8 @@ fun main () {
     }
     println("----------------")
 
-    println("Maior salário é: ${salarios.max()}")
-    println ("Menor salário é: ${salarios.min()}")
+    println("Maior salário é: ${salarios.maxOrNull()}")
+    println ("Menor salário é: ${salarios.minOrNull()}")
     println ("Média dos salários: ${salarios.average()}")
 
      val salariosMaiorQue1500 = salarios.filter { it >= 1500.0 }
