1abdfa9fd0dff77207e92a6ef21ab13c06862233
Lucas Stefano <lk.stefano1@gmail.com>
2021-03-25 22:50:31 -0300

    Created Arrays count, any, find class to test

---
+++ src/main/kotlin/one/digitalinnovation/collections/Operações.kt
@@ -0,0 +1,35 @@
+package one.digitalinnovation.collections
+
+fun main () {
+
+    val salarios = doubleArrayOf(1000.0, 1500.0, 4000.0)
+
+    for (salario in salarios) {
+        println(salario)
+    }
+    println("----------------")
+
+    println("Maior salário é: ${salarios.max()}")
+    println ("Menor salário é: ${salarios.min()}")
+    println ("Média dos salários: ${salarios.average()}")
+
+     val salariosMaiorQue1500 = salarios.filter { it >= 1500.0 }
+
+    println("----------------")
+
+    salariosMaiorQue1500.forEach { println(it) }
+
+    println("----------------")
+
+    println(salarios.count {it in 1000.0..4000.0})
+
+    println("----------------")
+
+    println(salarios.find {it == 1000.0})
+    println(salarios.find {it == 500.0})
+
+    println("----------------")
+    println(salarios.any {it == 1000.0})
+    println(salarios.any {it == 500.0})
+
+}
